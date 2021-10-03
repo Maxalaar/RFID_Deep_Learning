2c18a9e79e0466f58dc4108c4edcf8f0e1939045
Maxalaar <50994841+Maxalaar@users.noreply.github.com>
2021-10-03 14:03:39 +0200

    Update rfid_deep_learning_main.py

--- rfid_deep_learning_main.py
+++ rfid_deep_learning_main.py
@@ -7,8 +7,7 @@ from dense_neural_network_classifier import *
 
 environment_info()
 
-# create_tracking_dataset(2, 3, "debug_dataset", "debug_reccurent_simple_dataset", 2, num_repetitions_target=1, num_repetitions_zone=1, verbose=True)
-
+create_tracking_dataset(2, 3, "debug_dataset", "debug_reccurent_simple_dataset", 2, num_repetitions_target=1, num_repetitions_zone=1, verbose=True)
 create_tracking_dataset(10, 19, "kitchen_20cm", "debug_reccurrent_kitchen_20cm", 2, num_repetitions_target=1, num_repetitions_zone=1, verbose=True)
 
 # data_X, data_Y = load_tracking_dataset("debug_reccurent_dataset")
